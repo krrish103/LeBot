dd5c058900fdf7eac004f672fcb674dcd22248fe
hishaamabdul <hishaamdxb@gmail.com>
2021-05-08 21:49:11 +0400

    update

--- main.py
+++ main.py
@@ -4,15 +4,24 @@ import pyshorteners
 import smtplib
 from PIL import Image
 import png
+import language
+import json
+import random
 import pyqrcode
 from pyqrcode import QRCode
 from discord.ext import commands
-
-TOKEN = 'ODM0NzkzNDU3NjQ1Mzg3Nzg2.YIGD_g.kGs82liJuuDCHoStD2wE_g0f9Yo'
+import time
+import nmap
+import socket
+import requests
+TOKEN = "ODM0NzkzNDU3NjQ1Mzg3Nzg2.YIGD_g.kGs82liJuuDCHoStD2wE_g0f9Yo"
 
 # DEFINING THE BOT
-Bot = commands.Bot(command_prefix="-")
-client = discord.Client()
+Bot = commands.Bot(command_prefix="-", intents = discord.Intents.all())
+intents = discord.Intents.default()
+intents.members=True
+client = discord.Client(intents=intents)
+Bot.remove_command('help')
 
 
 # WHEN BOT IS ONLINE
@@ -24,11 +33,41 @@ async def on_ready():
     print("Bot is online")
 
 
+
+@Bot.event
+async def on_message_join(member):
+    channel = Bot.get_channel(channel_id)
+    embed=discord.Embed(title=f"Welcome {member.name}", description=f"Thanks for joining {member.guild.name}!")
+    embed.set_thumbnail(url=member.avatar_url)
+
+    await channel.send(embed=embed)
+
 @Bot.event
 async def on_message(message):
-    await Bot.process_commands(message)
-    if message.content == "-hello":
+  await Bot.process_commands(message)
+  if message.content == "-hello":
         await message.author.send("hello to you too")
+  if message.content == "-hello":
+      await message.author.send("hello to you too")
+  if message.content.startswith('-help'):
+    embed = discord.Embed(
+    color = discord.Colour.from_rgb(48, 37, 84)
+   )
+    embed.set_author(name="Help Command")
+    embed.add_field(name="Command Prefix: ", value="-", inline=False)
+    embed.add_field(name="avatar", value="Gets the Avatar of a User", inline=False)
+    embed.add_field(name="id", value="Gets the ID of a User", inline=False)
+    embed.add_field(name="shorten", value="Shortens the URL Provided", inline=False)
+    embed.add_field(name="qrcode", value="Generate a QR Code  for the Text or LInk Provided", inline=False)
+    embed.add_field(name="inspire", value="Sends An Inspirational Quote", inline=False)
+    embed.add_field(name="help", value="Shows This Message", inline=False)
+    embed.add_field(name="hi", value="Hello?", inline=False)
+    embed.add_field(name="hello", value="DM's you back", inline=False)
+    embed.add_field(name="dev", value="The Developer of the   Bot", inline=False)
+    embed.add_field(name="invite", value="Invite the Bot to your Server!", inline=False)
+    await message.channel.send(embed=embed)
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
 
 
 # COMMAND
@@ -36,11 +75,37 @@ async def on_message(message):
 async def hi(ctx):
     await ctx.send("Hello")
 
+@Bot.command()  # BRACKETS
+async def invite(ctx):
+    embed = discord.Embed()
+    embed.description = "<:invite:839052831536709694> ** [Click Here](http://bit.ly/Lebotinvite)** to Invite **LeBot** to your server!."
+    await ctx.send(embed=embed)
+
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@Bot.command()
+async def servers(ctx):
+    await ctx.channel.send("I'm in " + str(len(Bot.guilds)) + " servers!")
+
+@Bot.command()
+async def inspire(ctx):
+    quote = get_quote()
+    await ctx.send(quote)
+
 
 @Bot.command()  # BRACKETS
 async def dev(ctx):
-    await ctx.send("AspireFlight#0946")
+    await ctx.send("<:aspireflight:840579032235769876> AspireFlight#0946")
 
+
+@Bot.command()
+async def ping(ctx):
+    await ctx.send('My Ping is {0}'.format(round(Bot.latency, 1)))
 
 @Bot.command()
 async def qrcode(ctx, *, link):
@@ -55,7 +120,6 @@ async def shorten(ctx, *, link):
     x = shortner.isgd.short(link)
     print(x)
     await ctx.send(x)
-
 
 
 @Bot.command()  # BRACKETS
